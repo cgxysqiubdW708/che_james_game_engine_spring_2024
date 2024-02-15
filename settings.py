ed3b280ca70c71462f26bd50f0aada5b7aa79ed3
James <146895449+cgxysqiubdW708@users.noreply.github.com>
2024-02-15 08:58:23 -0800

    Hello Upload!

---
+++ settings.py
@@ -0,0 +1,23 @@
+# This file was created by: James Che
+
+# define values for width and height
+WIDTH = 1024
+HEIGHT = 768
+
+# set frames per second
+FPS = 30
+
+# define the title of the game
+TITLE = "My Fun Game"
+
+# define the size of the game's title
+TILESIZE = 32
+
+# define RGB values for color variables used in the game
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+LIGHTGREY = (150, 150, 150)
+BGCOLOR = (0, 0, 0)
+
+# Player settings
+PLAYER_SPEED = 300
